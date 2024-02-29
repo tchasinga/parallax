f50447574227c19a4ff9e4cef0655f2176e3dfc5
Tchasinga jacques <tchasingajacques@gmail.com>
2024-02-29 11:44:44 +0200

    new code is added now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
-import './App.css'
+import "./App.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
-
   return (
     <>
+      <Parallax pages={4}>
+        <ParallaxLayer>
+          <div className="codeOne">
+            <h1>New is added now</h1>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer>
+          <div className="codeOne second">
+            <h1>Get new Mian point</h1>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer>
+          <div className="codeOne three">
+            <h1>Post request is added</h1>
+          </div>
+        </ParallaxLayer>
+
+        <ParallaxLayer>
+          <div className="codeOne last">
+            <h1>Great main musics</h1>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+{
+  /*
+
+
+
+
+
+*/
+}
