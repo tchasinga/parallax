182a8b388501e128ccd16640b7064c4ebe8f9790
Tchasinga jacques <tchasingajacques@gmail.com>
2024-04-02 11:31:20 +0200

    New code is added

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,7 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 function App() {
   return (
     <>
-      <Parallax pages={4}>
-
-      <ParallaxLayer  >
-      <div className="codeOne">
-        <h1>New is added now</h1>
-      </div>
-      </ParallaxLayer>
-
-      <ParallaxLayer >
-      <div className="codeOne second">
-        <h1>Get new Main point</h1>
-      </div>
-      </ParallaxLayer>
-
-     <ParallaxLayer  >
-     <div className="codeOne three">
-        <h1>Post request is added</h1>
-      </div>
-     </ParallaxLayer>
-
-      <ParallaxLayer  >
-      <div className="codeOne last">
-        <h1>Great main musics</h1>
-      </div>
-      </ParallaxLayer>
-      </Parallax>
+       
     </>
   );
 }
