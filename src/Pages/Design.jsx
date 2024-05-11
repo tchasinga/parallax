cb3414bd352c13c923cf24f7d0e6960175e2b2f4
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-11 09:06:46 +0200

    new code is added now

--- src/Pages/Design.jsx
+++ src/Pages/Design.jsx
@@ -16,7 +16,7 @@ export default function Design() {
   return (
     <div>
       <h1
-        className=""
+        className="text-4xl text-center font-bold text-blue-500 hover:text-red-500 duration-700 border-2 border-teal-800 mt-5 mx-auto max-w-4xl p-5 rounded-lg"
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
       >
