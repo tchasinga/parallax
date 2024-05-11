7f1be15feff9f96d72dea9f88917446380d7a489
Tchasinga jacques <tchasingajacques@gmail.com>
2024-05-11 08:16:49 +0200

    new codeis added now

--- src/Pages/Design.jsx
+++ src/Pages/Design.jsx
@@ -2,19 +2,19 @@ import { useState } from "react";
 
 export default function Design() {
   const [hoverState, setHoverState] = useState("hover is not there");
+  const letterMessage = "hover is not there"
 
   const handleMouseOver = () => {
     setHoverState("the best way to hover");
   };
 
   const handleMouseOut = () => {
-    setHoverState("hover is not there");
+    setHoverState(letterMessage);
   };
 
   return (
     <div>
-      <h1
-        className=""
+      <h1 className=""
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
       >
